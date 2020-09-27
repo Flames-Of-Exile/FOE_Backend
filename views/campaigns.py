5bf1f0dc148f6ac274e8e8ec4d75b8e8cc319d93
Mark Howard <mark.c.howard.19@gmail.com>
2020-09-27 11:45:49 -0500

    simple endpoints added

--- views/campaigns.py
+++ views/campaigns.py
@@ -3,3 +3,34 @@ from flask import current_app, Blueprint, jsonify, request, Response
 from models import db, Campaign
 
 campaigns = Blueprint('campaigns', __name__, url_prefix='/api/campaigns')
+
+@campaigns.route('', methods=['GET'])
+def ListCampaigns():
+    return jsonify([campaign.to_dict() for campaign in Campaign.query.all()])
+
+@campaigns.route('', methods=['POST'])
+def CreateCampaign():
+    json = request.json
+    try:
+        newCampaign = Campaign(json['name'], json['image'])
+        db.session.add(newCampaign)
+        db.session.commit()
+        return Response("success", status=201, mimetype='application/json')
+    except:
+        return Response("error", status=400, mimetype='application/json')
+
+@campaigns.route('/<id>', methods=['GET'])
+def RetrieveCampaign(id=0):
+    return jsonify(Campaign.query.get_or_404(id).to_dict())
+
+@campaigns.route('/<id>', methods=['PATCH'])
+def UpdateCampaign(id=0):
+    campaign = Campaign.query.get_or_404(id)
+    try:
+        json = request.json
+        campaign.name = json['name']
+        campaign.image = json['image']
+        db.session.commit()
+        return jsonify(campaign.to_dict())
+    except:
+        return Response('error', status=400, mimetype='application/json')
--- views/edits.py
+++ views/edits.py
@@ -3,3 +3,22 @@ from flask import current_app, Blueprint, jsonify, request, Response
 from models import db, Edit
 
 edits = Blueprint('edits', __name__, url_prefix='/api/edits')
+
+@edits.route('', methods=['GET'])
+def ListEdits():
+    return jsonify([edit.to_dict() for edit in Edit.query.all()])
+
+@edits.route('', methods=['POST'])
+def CreateEdit():
+    json = request.json
+    #try:
+    newEdit = Edit(json['details'], json['pin']['id'], json['user']['id'])
+    db.session.add(newEdit)
+    db.session.commit()
+    return Response("success", status=201, mimetype='application/json')
+    #except:
+    #    return Response("error", status=400, mimetype='application/json')
+
+@edits.route('/<id>', methods=['GET'])
+def RetrieveEdit(id=0):
+    return jsonify(Edit.query.get_or_404(id).to_dict())
