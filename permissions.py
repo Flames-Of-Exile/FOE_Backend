4b32c782b2de2d726c570204afc51f670406cbf4
Mark Howard <mark.c.howard.19@gmail.com>
2020-09-29 15:47:56 -0500

    creating/updating a pin creates an edit object, plus various fixes

--- permissions.py
+++ permissions.py
@@ -9,7 +9,9 @@ def is_administrator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         user = User.query.get(get_jwt_identity()['id'])
-        if (user.role != User.Role.ADMIN.value):
+        if (not user.is_active):
+            return Response('account is locked', status=403)
+        if (user.role not in [User.Role.ADMIN]):
             return Response('requires administrator account', status=403)
         return func(*args, **kwargs)
     return wrapper
@@ -18,7 +20,9 @@ def is_member(func, **kwargs):
     @wraps(func)
     def wrapper(*args, **kwargs):
         user = User.query.get(get_jwt_identity()['id'])
-        if (user.role in [User.Role.MEMBER.value, User.Role.ADMIN.value]):
+        if (not user.is_active):
+            return Response('account is locked', status=403)
+        if (user.role not in [User.Role.MEMBER, User.Role.ADMIN]):
             return Response('requires member account', status=403)
         return func(*args, **kwargs)
     return wrapper
--- views/edits.py
+++ views/edits.py
@@ -7,11 +7,13 @@ from permissions import is_administrator, is_member
 edits = Blueprint('edits', __name__, url_prefix='/api/edits')
 
 @edits.route('', methods=['GET'])
+@jwt_required
 @is_member
 def ListEdits():
     return jsonify([edit.to_dict() for edit in Edit.query.all()])
 
 @edits.route('', methods=['POST'])
+@jwt_required
 @is_member
 def CreateEdit():
     json = request.json
@@ -26,6 +28,7 @@ def CreateEdit():
         return Response('error creating record', status=400)
 
 @edits.route('/<id>', methods=['GET'])
+@jwt_required
 @is_member
 def RetrieveEdit(id=0):
     return jsonify(Edit.query.get_or_404(id).to_dict())
