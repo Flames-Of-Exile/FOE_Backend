48d3fe793fdbfb41b1edd147223e6ada7705985f
Mark Howard <mark.c.howard.19@gmail.com>
2020-11-16 16:34:48 -0600

    fixed migration problem

--- migrations/env.py
+++ migrations/env.py
@@ -22,6 +22,7 @@ logger = logging.getLogger('alembic.env')
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+from models import Campaign, Edit, Guild, Pin, User, World
 from flask import current_app
 config.set_main_option(
     'sqlalchemy.url', current_app.config.get(
